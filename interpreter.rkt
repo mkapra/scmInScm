dca462414aae091e4e74d25d9a9e4be43b3d0e85
Maximilian Kapra <maximilian@kapra.de>
2022-07-14 14:27:41 +0200

    Add some constructors and getters/Fix debug output

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,29 +1,70 @@
-(define memSize 1000)
+(define memSize 100)
 
 ;; The memory where all the data is stored
 (define mem (make-vector memSize 0))
 ;; Pointer that points to the next free index in the memory vector
 (define free 0)
 
-(define (error e)
-  (display e)
-  (newline)
-  exit
+;;;;;;;;;;;;;;;;;; Getter ;;;;;;;;;;;;;;;;;;
+(define (tag? n t)
+  (eq? (vector-ref mem n) t)
   )
 
-;; Allocates a specific amount (n) of memory in the vector
-;; This is simply done by increasing the free pointer
-(define (malloc n)
-  (if (> (+ n free 1) memSize)
-      (error "Memory Overflow in malloc")
-      (begin
-        (ref! free 0 n)
-        (set! free (+ free n 1))
-        (- free n)
-        )
+(define (i-pair? n)
+  (tag? n 'pair)
+  )
+
+(define (number->value p)
+  (if (tag? p 'number)
+      (ref p 1)
+      (error "Pointer is not a number")
       )
   )
 
+(define (i-car p)
+  (if (tag? p 'pair)
+      (ref p 1)
+      (error "Pointer is not a pair")
+      )
+  )
+
+(define (i-cdr p)
+  (if (tag? p 'pair)
+      (ref p 2)
+      (error "Pointer is not a pair")
+      )
+  )
+
+(define (symbol->name p)
+  (if (tag? p 'symbol)
+      (ref p 1)
+      (error "Pointer is not a symbol")
+      )
+  )
+
+;;;;;;;;;;;;;;;;;; Constructors ;;;;;;;;;;;;;;;;;;
+(define (i-number value)
+  (define ptr (malloc 2))
+  (ref! ptr 0 'number)
+  (ref! ptr 1 value)
+  ptr
+  )
+
+(define (i-cons h t)
+  (define ptr (malloc 3))
+  (ref! ptr 0 'pair)
+  (ref! ptr 1 h)
+  (ref! ptr 2 t)
+  ptr
+  )
+
+(define (i-symbol v)
+  (define ptr (malloc 2))
+  (ref! ptr 0 'symbol)
+  (ref! ptr 1 v)
+  ptr
+  )
+        
 ;;;;;;;;;;;;;;;;;; Memory Dump ;;;;;;;;;;;;;;;;;;
 ;; Prints the content of the memory to the console in an understandable format
 (define (dumpMem)
@@ -32,15 +73,6 @@
       (dumpMemRek 0)
       )
   )
-
-(define (ref p i)
-  (vector-ref mem (+ p i 1))
-  )
-
-(define (ref! p i v)
-  (vector-set! mem (+ p i) v)
-  )
-
 
 (define (dumpObjArgs begin end)
   (display " - ")
@@ -65,17 +97,53 @@
              (argBegin (+ i 2))
              (argEnd (+ argBegin (- recLen 2)))
              )
-        (display tagIndex) (display ": ") (display (vector-ref mem tagIndex))
-             
-        (if (and (not (= recLen 1)) (not (= recLen 0)))
-            (dumpObjArgs argBegin argEnd)
+        (if (> recLen 0)
             (begin
-              (newline)
-              (dumpMemRek (+ i (vector-ref mem i) 1))
+              (display tagIndex) (display ": ") (display (vector-ref mem tagIndex))
+
+              ;; Print args only if they exist (this is the case if the recLen is greater than 1
+              (if (or (> recLen 1))
+                  (dumpObjArgs argBegin argEnd)
+                  (begin
+                    (newline)
+                    )
+                  )
               )
             )
+        
+        (dumpMemRek (+ i (vector-ref mem i) 1))
         )
       )
+  )
+
+;;;;;;;;;;;;;;;;;; Memory operations ;;;;;;;;;;;;;;;;;;
+(define (ref p i)
+  (vector-ref mem (+ p i))
+  )
+
+(define (ref! p i v)
+  (vector-set! mem (+ p i) v)
+  )
+
+;; Allocates a specific amount (n) of memory in the vector
+;; This is simply done by increasing the free pointer
+(define (malloc n)
+  (if (> (+ n free 1) memSize)
+      (error "Memory Overflow in malloc")
+      (begin
+        (ref! free 0 n)
+        (set! free (+ free n 1))
+        (- free n)
+        )
+      )
+  )
+
+(define (error e)
+  (newline)
+  (display "ERROR: ")
+  (display e)
+  (newline)
+  exit
   )
 
 ;;;;;;;;;;;;;;;;;; TESTS ;;;;;;;;;;;;;;;;;;
@@ -96,9 +164,24 @@
   (ref! ptr 0 'false)
   (set! ptr (malloc 3))
   (ref! ptr 0 'pair)
+  (ref! ptr 1 1)
+  (ref! ptr 2 2)
   (set! ptr (malloc 1))
   (ref! ptr 0 'false)
   (dumpMem)
   )
 
-(setTest)
+(define (consTest)
+  (let* ((nr1 (i-number 200))
+         (nr2 (i-number 50))
+         (nr3 (i-number 70))
+         (p1 (i-cons nr1 nr2))
+         (p2 (i-cons p1 nr3))
+         )
+    (dumpMem)
+    ;;(display (number->value (i-car (i-car p2))))(newline)
+    ;;(display (number->value (i-cdr p2)))
+    )
+  )
+
+(consTest)
